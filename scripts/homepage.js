c082b3bdc4e328b193e94474b2369cf401bb003e
Froylan <Froylan@dev.com>
2022-11-07 11:23:49 -0600

    test java script script

---
+++ scripts/homepage.js
@@ -0,0 +1,8 @@
+const saveFile = require("fs").writeFileSync;
+const pkgJsonPath =
+  require.main.paths[1].split("node_modules")[0] + "package.json";
+const json = require(pkgJsonPath);
+if (json.hasOwnProperty("homepage")) {
+  json.homepage = "https://froy-qa-env.netlify.app/";
+}
+saveFile(pkgJsonPath, JSON.stringify(json, null, 2));
